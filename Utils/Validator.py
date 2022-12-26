b8fee33e50e3a91d2756e970211afce5cfd53487
Giovanni Sullutrone <giovisull@gmail.com>
2022-12-26 20:01:59 +0100

    Added comments to the training routine and modified validator again to better incorporate different forward functions

--- Utils/Validator.py
+++ Utils/Validator.py
@@ -6,13 +6,12 @@ from torch.nn import functional as F
 
 
 class Validator:
-    def __init__(self, custom_dataset: CustomDataset, label_function, forward_function, batch_size: int=32) -> None:
+    def __init__(self, custom_dataset: CustomDataset, label_function, batch_size: int=32) -> None:
         dm_val: CustomDataLoader = CustomDataLoader(custom_dataset)
         self.dl_val = dm_val.get_unbalanced_dataloader(batch_size=batch_size, shuffle=False)
         self.label_function = label_function
-        self.forward_function = forward_function
     
-    def validate(self):
+    def validate(self, forward_function):
         ae_for_age_group = {x: [] for x in range(8)}
 
         errors = []
@@ -23,7 +22,7 @@ class Validator:
                     x = x.to("cuda")
                     y = y.to("cuda")
 
-                    out = self.forward_function(x)
+                    out = forward_function(x)
 
                     for y_real, y_pred in zip(self.label_function(y), self.label_function(out)):
                         ae = torch.abs(y_real - y_pred)

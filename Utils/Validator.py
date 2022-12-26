f68e85672c65130144b6e363f1442c7713bdcc20
Giovanni Sullutrone <giovisull@gmail.com>
2022-12-26 19:56:38 +0100

    Modified validator to work with different architectures

--- Utils/Validator.py
+++ Utils/Validator.py
@@ -6,12 +6,13 @@ from torch.nn import functional as F
 
 
 class Validator:
-    def __init__(self, custom_dataset: CustomDataset, label_function, batch_size: int=32) -> None:
+    def __init__(self, custom_dataset: CustomDataset, label_function, forward_function, batch_size: int=32) -> None:
         dm_val: CustomDataLoader = CustomDataLoader(custom_dataset)
         self.dl_val = dm_val.get_unbalanced_dataloader(batch_size=batch_size, shuffle=False)
         self.label_function = label_function
+        self.forward_function = forward_function
     
-    def validate(self, model_age_group, model_age):
+    def validate(self):
         ae_for_age_group = {x: [] for x in range(8)}
 
         errors = []
@@ -22,14 +23,7 @@ class Validator:
                     x = x.to("cuda")
                     y = y.to("cuda")
 
-                    knowledge_age_group = torch.tensor([[0.33, 0.33, 0.33]]*len(x), requires_grad=False).float().to("cuda")
-
-                    knowledge = model_age_group(x, knowledge_age_group)
-
-                    knowledge = F.softmax(knowledge, dim=-1)
-
-                    out = model_age(x, knowledge)
-                    out = F.softmax(out, dim=-1)
+                    out = self.forward_function(x)
 
                     for y_real, y_pred in zip(self.label_function(y), self.label_function(out)):
                         ae = torch.abs(y_real - y_pred)

6e2126851f80ca0f1c672e07703a45b8c0b43a41
Giovanni Sullutrone <giovisull@gmail.com>
2023-01-07 16:40:12 +0100

    Cose

--- Utils/Validator.py
+++ Utils/Validator.py
@@ -170,3 +170,48 @@ class Validator:
         sigma = torch.sqrt(sigma)
         
         return (ae_for_age_group, mae, torch.max(torch.tensor(0), 5 - mmae) + torch.max(torch.tensor(0), 5 - sigma), torch.max(torch.tensor(0), 7 - mae) + torch.max(torch.tensor(0), 3 - sigma))
+
+    def validate_ext3(self, forward_function):
+        ae_for_age_group = {x: [] for x in range(8)}
+        ae_for_age = {x: [] for x in range(81)}
+
+        errors = []
+        with tqdm(self.dl_val, unit=" batch") as tepoch:
+            for batch in tepoch:
+                with torch.no_grad():
+                    x, y = batch
+                    x = x.to("cuda")
+                    y = y.to("cuda")
+
+                    out = forward_function(x)
+
+                    for y_real, y_pred in zip(self.label_function(y), self.label_function(out)):
+                        y_pred = torch.round(y_pred)
+                        ae = torch.abs(y_real - y_pred)
+                        errors.append(ae)
+
+                        idx = 7 if int((y_real)/10) > 7 else int((y_real)/10)
+
+                        ae_for_age_group[idx] += [ae]
+                        ae_for_age[int(y_real)] += [ae]
+        # MAE
+        mae = torch.mean(torch.tensor(errors))
+        
+        # MMAE
+        mmae = 0
+        for k in ae_for_age_group:
+            ae_for_age_group[k] = torch.mean(torch.tensor(ae_for_age_group[k]))
+            mmae += ae_for_age_group[k]
+        mmae = mmae / len(ae_for_age_group)
+
+        for k in ae_for_age:
+            ae_for_age[k] = torch.mean(torch.tensor(ae_for_age[k]))
+
+        # SIGMA
+        sigma = 0
+        for k in ae_for_age_group:
+            sigma += torch.square(ae_for_age_group[k] - mae)
+        sigma = sigma / len(ae_for_age_group)
+        sigma = torch.sqrt(sigma)
+        
+        return (ae_for_age, ae_for_age_group, mae, torch.max(torch.tensor(0), 5 - mmae) + torch.max(torch.tensor(0), 5 - sigma), torch.max(torch.tensor(0), 7 - mae) + torch.max(torch.tensor(0), 3 - sigma))
